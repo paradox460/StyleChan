b050b5273649c1ab7b0ff61b192fe7a65709eaa5
nebukazar <justsoulay@gmail.com>
2016-09-20 22:59:02 +0100

    Release StyleChan v1.0.4

--- builds/StyleChan.meta.js
+++ builds/StyleChan.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         StyleChan
-// @version      1.0.3
+// @version      1.0.4
 // @namespace    StyleChan
 // @description  Customizable themes for 4chan X.
 // @minGMVer     1.14
